30e82a209d20c8bbba58f163cd75d93c480ac0b8
bagrounds <bagrounds@gmail.com>
2018-01-28 03:59:47 -0800

    [MINOR][ADD] hyloR, paraR, apoR

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@
   const { inputs } = require('guarded')
   const { map: oMap, ap: oAp } = require('fun-object')
   const { any, tuple, hasFields, fun } = require('fun-type')
-  const { pipeAll, composeAll } = require('fun-function')
+  const { id, pipeAll, composeAll } = require('fun-function')
+  const { split } = require('fun-arrow')
 
   /**
    * unfix >>> map(cata({ map, unfix }, alg)) >>> alg
@@ -29,6 +30,89 @@
    */
   const cataR = ({ map, unfix }, alg, x) =>
     pipeAll([unfix, curry(map)(curry(cataR)({ map, unfix }, alg)), alg])(x)
+
+  /**
+   * fix <<< map(ana({ map, fix }, coalg)) <<< coalg
+   *
+   * Recursive implementation (not stack safe)
+   *
+   * @function module:recursion-schemes.anaR
+   *
+   * @param {Object} term - Functorial, recursive data type
+   * @param {Function} term.map - (a -> b, f a) -> f b
+   * @param {Function} term.fix - f (term f) -> term f
+   * @param {Function} coalg - a -> f a
+   * @param {*} x - seed to build structure from
+   *
+   * @return {*} x built up by recursively applying coalg to x
+   */
+  const anaR = ({ map, fix }, coalg, x) =>
+    composeAll([fix, curry(map)(curry(anaR)({ map, fix }, coalg)), coalg])(x)
+
+  /**
+   * alg <<< map(hylo({ map }, alg, coalg)) <<< coalg
+   *
+   * Recursive implementation (not stack safe)
+   *
+   * @function module:recursion-schemes.hyloR
+   *
+   * @param {Object} term - Functorial, recursive data type
+   * @param {Function} term.map - (a -> b, f a) -> f b
+   * @param {Function} alg - f a -> a
+   * @param {Function} coalg - a -> f a
+   * @param {*} x - seed to build structure from
+   *
+   * @return {*} x built up by recursively applying coalg to x
+   */
+  const hyloR = ({ map }, alg, coalg, x) =>
+    composeAll([alg, curry(map)(curry(hyloR)({ map }, alg, coalg)), coalg])(x)
+
+  /**
+   * unfix >>> map(id &&& para({ map, unfix }, ralg)) >>> ralg
+   *
+   * Recursive implementation (not stack safe)
+   *
+   * @function module:recursion-schemes.paraR
+   *
+   * @param {Object} term - Functorial, recursive data type
+   * @param {Function} term.map - (a -> b, f a) -> f b
+   * @param {Function} term.unfix - term f -> f (term f)
+   * @param {Function} ralg - f (term f, a) -> a
+   * @param {*} x - structure to reduce
+   *
+   * @return {*} x reduced by recursively applying ralg to (term f, a)
+   */
+  const paraR = ({ map, unfix }, ralg, x) =>
+    pipeAll([
+      unfix,
+      curry(map)(split(id, curry(paraR)({ map, unfix }, ralg))),
+      ralg
+    ])(x)
+
+  /**
+   * fix <<< map(id ||| apo({ map, unfix }, rcoalg)) <<< rcoalg
+   *
+   * Recursive implementation (not stack safe)
+   *
+   * @function module:recursion-schemes.apoR
+   *
+   * @param {Object} term - Functorial, recursive data type
+   * @param {Function} term.map - (a -> b, f a) -> f b
+   * @param {Function} term.fix - term f -> f (term f)
+   * @param {Function} term.either -
+   *   (term f -> term f, a -> term f) -> (term f | a) -> term f
+   *
+   * @param {Function} rcoalg - a -> f (term f | a)
+   * @param {*} x - seed to build structure from
+   *
+   * @return {*} x built up by recursively applying rcoalg to x
+   */
+  const apoR = ({ map, fix, either }, rcoalg, x) =>
+    composeAll([
+      fix,
+      curry(map)(either(id, curry(apoR)({ map, fix, either }, rcoalg))),
+      rcoalg
+    ])(x)
 
   /**
    * unfix >>> map(cata({ map, unfix }, alg)) >>> alg
@@ -66,31 +150,16 @@
     return alg(q[0])
   }
 
-  /**
-   * fix <<< map(ana({ map, fix }, coalg)) <<< coalg
-   *
-   * Recursive implementation (not stack safe)
-   *
-   * @function module:recursion-schemes.anaR
-   *
-   * @param {Object} term - Functorial, recursive data type
-   * @param {Function} term.map - (a -> b, f a) -> f b
-   * @param {Function} term.fix - f (term f) -> term f
-   * @param {Function} coalg - a -> f a
-   * @param {*} x - seed to build structure from
-   *
-   * @return {*} x built up by recursively applying coalg to x
-   */
-  const anaR = ({ map, fix }, coalg, x) =>
-    composeAll([fix, curry(map)(curry(anaR)({ map, fix }, coalg)), coalg])(x)
-
   /* exports */
-  const api = { cataR, cata, anaR }
+  const api = { cataR, anaR, hyloR, paraR, apoR, cata }
 
   const guards = oMap(inputs, {
     cataR: tuple([hasFields({ map: fun, unfix: fun }), fun, any]),
-    cata: tuple([hasFields({ map: fun, unfix: fun }), fun, any]),
-    anaR: tuple([hasFields({ map: fun, fix: fun }), fun, any])
+    anaR: tuple([hasFields({ map: fun, fix: fun }), fun, any]),
+    hyloR: tuple([hasFields({ map: fun }), fun, fun, any]),
+    paraR: tuple([hasFields({ map: fun, unfix: fun }), fun, any]),
+    apoR: tuple([hasFields({ map: fun, fix: fun, either: fun }), fun, any]),
+    cata: tuple([hasFields({ map: fun, unfix: fun }), fun, any])
   })
 
   module.exports = oMap(curry, oAp(guards, api))
--- test/tests.js
+++ test/tests.js
@@ -3,14 +3,14 @@
 
   /* imports */
   const arrange = require('fun-arrange')
-  const { equal, equalDeep } = require('fun-predicate')
+  const { and, equal, equalDeep } = require('fun-predicate')
   const { sync } = require('fun-test')
-  const { ap, get } = require('fun-object')
+  const { ap, get: oGet } = require('fun-object')
   const { gt, add, mul } = require('fun-scalar')
-  const { map, update, drop, first, range, prepend, append, flatten } =
-    require('fun-array')
+  const { map, update, drop, first, range, prepend, append, flatten, get,
+    flattenR } = require('fun-array')
   const { compose, id, k, argsToArray } = require('fun-function')
-  const { tuple, array, num, string } = require('fun-type')
+  const { any, tuple, array, num, string } = require('fun-type')
   const { fromArray, isEmpty: isNil, empty: nil } = require('fun-list')
   const { first: poly } = require('fun-case')
   const { split, parallel: both } = require('fun-arrow')
@@ -23,7 +23,12 @@
   const listF = {
     map: listMap,
     unfix: id,
-    fix: id
+    fix: id,
+    either: (l, r) => poly([
+      [tuple([equal('left'), any]), update(1, l)],
+      [tuple([equal('right'), any]), update(1, r)],
+      [k(true), x => { throw Error(`non-total matching ${JSON.stringify(x)}`) }]
+    ])
   }
 
   const exprF = (() => {
@@ -48,13 +53,33 @@
       [tuple([equal('max'), array]), ([_, xs]) => `max(${xs})`]
     ])
 
+    const paraPrint = poly([
+      [tuple([any, num]), id],
+      [
+        tuple([any, tuple([equal('add'), array])]),
+        ([x, [_, [a, b]]]) => `[${flattenR(x).length}](${a} + ${b})`
+      ],
+      [
+        tuple([any, tuple([equal('sub'), array])]),
+        ([x, [_, [a, b]]]) => `[${flattenR(x).length}](${a} - ${b})`
+      ],
+      [
+        tuple([any, tuple([equal('mul'), array])]),
+        ([x, [_, [a, b]]]) => `[${flattenR(x).length}](${a} * ${b})`
+      ],
+      [
+        tuple([any, tuple([equal('max'), array])]),
+        ([x, [_, xs]]) => `[${flattenR(x).length}]max(${xs})`
+      ]
+    ])
+
     const unfix = id
     const emap = (f, e) => poly([
       [num, id],
       [tuple([string, array]), update(1, map(f))]
     ])(e)
 
-    return { max, add, sub, mul, map: emap, compute, unfix, print }
+    return { max, add, sub, mul, map: emap, compute, unfix, print, paraPrint }
   })()
 
   const arrayF = {
@@ -63,11 +88,17 @@
     fix: poly([[isNil, id], [k(true), argsToArray(prepend)]])
   }
 
-  const natTerm = {
+  const natF = {
     map: listMap,
+    fix: ([_, [_1, x]]) => x,
     unfix: poly([
       [equalDeep(0), k([])],
       [k(true), n => [1, n - 1]]
+    ]),
+    either: (l, r) => poly([
+      [tuple([equal('left'), any]), update(1, l)],
+      [tuple([equal('right'), any]), update(1, r)],
+      [k(true), x => { throw Error(`non-total matching ${JSON.stringify(x)}`) }]
     ])
   }
 
@@ -87,6 +118,33 @@
     [x => x.length === 1, ([x]) => x],
     [k(true), argsToArray(add)]
   ])
+
+  const tails = poly([
+    [isNil, k([nil(), nil()])],
+    [k(true), ([a, [as, tls]]) => [[a, as], tls]]
+  ])
+
+  const fac = poly([
+    [isNil, k(1)],
+    [k(true), ([_, [n, m]]) => (1 + n) * m]
+  ])
+
+  const paraSum = poly([
+    [isNil, k(0)],
+    [k(true), compose(sum, update(1, get(1)))]
+  ])
+
+  const ite = (p, t, f, x) => p(x) ? t(x) : f(x)
+  const left = x => ['left', x]
+  const right = x => ['right', x]
+  const findNat = p => x =>
+    ite(
+      compose(p, get(1)),
+      update(1, left),
+      update(1, right),
+      split(id, add(1))(x)
+    )
+  const isSquare = x => Math.sqrt(x) === Math.floor(Math.sqrt(x))
 
   const upTo = n => poly([
     [gt(n), nil],
@@ -120,7 +178,7 @@
 
   const tests = map(
     compose(
-      ap({ predicate: equalDeep, contra: get }),
+      ap({ predicate: equalDeep, contra: oGet }),
       arrange({ inputs: 0, predicate: 1, contra: 2 })
     ),
     flatten([
@@ -135,7 +193,7 @@
         [[listF, sumOfSquares, fromArray([1, 2, 3])], 14],
         [[arrayF, sum, [1, 2, 3]], 6],
         [[arrayF, prod, [2, 3, 4]], 24],
-        [[natTerm, sum, 10], 10],
+        [[natF, sum, 10], 10],
         [[treeTerm, sum, [[], [[1], [[2], [3]]]]], 6],
         [[exprF, exprF.compute, expr1], 9],
         [[exprF, exprF.print, expr1], '((2 * 3) + (5 - 2))'],
@@ -147,6 +205,28 @@
       map(append('anaR'), [
         [[listF, upTo(3), 0], [0, [1, [2, [3, []]]]]],
         [[arrayF, upTo(3), 0], [0, 1, 2, 3]]
+      ]),
+      map(append('hyloR'), [
+        [[listF, sum, upTo(0), 0], 0],
+        [[listF, sum, upTo(10), 0], 55],
+        [[listF, sum, upTo(100), 0], 5050]
+      ]),
+      map(append('paraR'), [
+        [[listF, paraSum, []], 0],
+        [[listF, paraSum, fromArray(range(0, 10))], 55],
+        [[listF, paraSum, fromArray(range(0, 10))], 55],
+        [
+          [listF, tails, fromArray(range(1, 3))],
+          [[1, [2, [3, []]]], [[2, [3, []]], [[3, []], [[], []]]]]
+        ],
+        [[arrayF, paraSum, range(0, 10)], 55],
+        [[natF, fac, 3], 6],
+        [[natF, fac, 4], 24],
+        [[natF, fac, 10], 3628800]
+      ]),
+      map(append('apoR'), [
+        [[natF, findNat(and(gt(10), isSquare)), 0], 16],
+        [[listF, findNat(equal(1)), 0], [0, ['left', 1]]]
       ])
     ])
   )
